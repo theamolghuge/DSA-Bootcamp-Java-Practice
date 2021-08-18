1ece69ac93804eac903e06be79e169337f6a5bd7
theamolghuge <theamolghuge@gmail.com>
2021-08-18 18:16:08 +0530

    added 2 more files

---
+++ 1-flow-of-program/src/com/LCMandHCF.java
@@ -0,0 +1,33 @@
+package com;
+
+import java.util.Scanner;
+
+public class LCMandHCF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 2 numbers: ");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		int num = Math.min(a, b);
+		for (int i = num; i > 1; i--) {
+			if (a % i == 0 && b % i == 0) {
+				System.out.println(i + "is HCF for " + a + " and " + b);
+				return;
+			} else {
+				System.out.println("There is no HCF");
+			}
+		}
+		for (int i = 2; i <= num; i++) {
+			if(a%i==0 && b%i==0) {
+				System.out.println(i+" is LCM");
+			}
+			else {
+				System.out.println("There is no LCM");
+			}
+		}
+	}
+
+}
---
+++ 1-flow-of-program/src/com/SumOfnNumbers.java
@@ -0,0 +1,12 @@
+package com;
+
+import java.util.Scanner;
+
+public class SumOfnNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
